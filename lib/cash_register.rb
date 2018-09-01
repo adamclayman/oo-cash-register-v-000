65f2079198667a894db22ac725f86fd52f8d47ac
Adam Clayman <adam@clayman.org>
2018-09-01 17:29:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     self.discount = discount
   end
 
-  def add_item(title, price, quantity = 1)
+  def self.add_item(title, price, quantity = 1)
     quantity.times do
       ITEMS << title
     end
