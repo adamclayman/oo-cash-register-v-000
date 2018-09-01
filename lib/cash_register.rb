6e0230199905f52adc7a2eedea2d2d8d562b56b1
Adam Clayman <adam@clayman.org>
2018-09-01 17:39:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,6 +21,7 @@ class CashRegister
       return "There is no discount to apply."
     else
       self.total = self.total * ((100.0 - self.discount) / 100.0)
+      return "After the discount, the total comes to #{self.total}."
     end
   end
 
